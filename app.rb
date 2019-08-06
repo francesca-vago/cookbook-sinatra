88c8901b76187118e71f87313837e67c8a1abc7a
Francesca Vago <vago.francesca@gmail.com>
2019-08-06 22:57:14 +0100

    Init Sinatra project

--- app.rb
+++ app.rb
@@ -1,11 +1,20 @@
-require "sinatra"
-require "sinatra/reloader" if development?
-require "pry-byebug"
-require "better_errors"
-configure :development do
-  use BetterErrors::Middleware
-  BetterErrors.application_root = File.expand_path('..', __FILE__)
-end
+require 'sinatra'
+# require 'sinatra/reloader' if development?
+# require 'pry-byebug'
+# require 'better_errors'
+
+# configure :development do
+#   use BetterErrors::Middleware
+#   BetterErrors.application_root = File.expand_path(__dir__)
+# end
+
+# get '/' do
+#   erb :index
+# end
+
+# get '/about' do
+#   erb :about
+# end
 
 get '/' do
   'Hello world!'
